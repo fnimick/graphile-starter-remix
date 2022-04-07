0adedbbec828dd708cd038c3da3b2dc35f0c66d6
Francis Nimick <fnimick@gmail.com>
2022-04-07 19:58:49 -0400

    Update eslint and vscode configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,6 +38,24 @@ module.exports = {
     react: {
       version: "detect",
     },
+    import: {
+      "import/ignore": ["node_modules", "\\.(css|md|svg|json)$"],
+      "import/parsers": {
+        [require.resolve("@typescript-eslint/parser")]: [
+          ".ts",
+          ".tsx",
+          ".d.ts",
+        ],
+      },
+      "import/resolver": {
+        [require.resolve("eslint-import-resolver-node")]: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+        [require.resolve("eslint-import-resolver-typescript")]: {
+          alwaysTryTypes: true,
+        },
+      },
+    },
   },
   env: {
     browser: true,
