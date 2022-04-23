85c0dfcba180be4f0d51e09f85d895534e031e08
Francis Nimick <fnimick@gmail.com>
2022-04-23 10:03:24 -0400

    Add forgot password

---
+++ @app/client/app/routes/__index/forgot.success.tsx
@@ -0,0 +1,23 @@
+import { Alert } from "antd";
+import { useSearchParams } from "remix";
+import { TypedDataFunctionArgs } from "~/utils/remix-typed";
+import { requireNoUser } from "~/utils/users";
+
+export const handle = { hideLogin: true, title: "Forgot Password" };
+
+export const loader = async ({ context }: TypedDataFunctionArgs) => {
+  await requireNoUser(context);
+  return null;
+};
+
+export default function ForgotPassword() {
+  const [searchParams] = useSearchParams();
+  const email = searchParams.get("email") as string;
+  return (
+    <Alert
+      type="success"
+      message="You've got mail"
+      description={`We've sent an email reset link to '${email}'; click the link and follow the instructions. If you don't receive the link, please ensure you entered the email address correctly, and check in your spam folder just in case.`}
+    />
+  );
+}
