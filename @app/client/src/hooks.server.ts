9211692a0e5879a5120d35a4bb52ee965f186f49
Francis Nimick <fnimick@gmail.com>
2023-02-11 16:58:41 -0500

    Correctly persist cookies in browser from graphql requests

--- @app/client/src/hooks.server.ts
+++ @app/client/src/hooks.server.ts
@@ -1,8 +1,9 @@
 import type { HandleFetch } from "@sveltejs/kit";
+import setCookie from "set-cookie-parser";
 
 import { PUBLIC_ROOT_URL } from "$env/static/public";
 
-export const handleFetch: HandleFetch = ({ event, request, fetch }) => {
+export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
   if (request.url.startsWith(PUBLIC_ROOT_URL)) {
     const cookie = event.request.headers.get("cookie");
     if (cookie) {
@@ -10,5 +11,20 @@ export const handleFetch: HandleFetch = ({ event, request, fetch }) => {
     }
   }
 
-  return fetch(request);
+  const response = await fetch(request);
+  if (request.url.startsWith(PUBLIC_ROOT_URL)) {
+    const cookieHeader = response.headers.get("set-cookie");
+    if (cookieHeader) {
+      for (const cookie of setCookie.parse(cookieHeader)) {
+        const { name, value, expires } = cookie;
+        event.cookies.set(name, value, {
+          expires,
+          httpOnly: true,
+          sameSite: "lax",
+          path: "/",
+        });
+      }
+    }
+  }
+  return response;
 };
