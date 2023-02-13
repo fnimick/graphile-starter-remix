93797306945919758599f1ba95ff1e01d87f87a2
Francis Nimick <fnimick@gmail.com>
2023-02-13 11:11:29 -0500

    Add logout route

---
+++ @app/client/src/routes/logout/+page.server.ts
@@ -0,0 +1,14 @@
+import { redirect } from "@sveltejs/kit";
+
+import { LogoutStore } from "$houdini";
+
+import type { Actions, RequestEvent } from "./$types";
+
+export const actions: Actions = {
+  default: async (event: RequestEvent) => {
+    const logoutMutation = new LogoutStore();
+
+    await logoutMutation.mutate(null, event);
+    throw redirect(302, "/");
+  },
+};
