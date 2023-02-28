2f0aaea573090e52ccd92a7c5f151dda718c9504
Francis Nimick <fnimick@gmail.com>
2023-02-28 13:39:07 -0500

    Rename to SettingsProfile query

--- @app/client/src/routes/(app)/settings/+layout.svelte
+++ @app/client/src/routes/(app)/settings/+layout.svelte
@@ -9,12 +9,12 @@
   // We use this rather than `data.currentUser` because the Houdini query
   // auto-updates along with any user record received from the
   // CurrentUserUpdated subscription.
-  const { SettingsProfileNew } = data;
+  const { SettingsProfile } = data;
 </script>
 
 <AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
   <svelte:fragment slot="sidebarLeft">
-    <Navigation currentUser={$SettingsProfileNew.data?.currentUser} />
+    <Navigation currentUser={$SettingsProfile.data?.currentUser} />
   </svelte:fragment>
   <slot />
 </AppShell>
--- @app/client/src/routes/(app)/settings/+layout.ts
+++ @app/client/src/routes/(app)/settings/+layout.ts
@@ -1,10 +1,10 @@
-import { load_SettingsProfileNew } from "$houdini";
+import { load_SettingsProfile } from "$houdini";
 
 import type { LayoutLoadEvent } from "./$types";
 
 export async function load(event: LayoutLoadEvent) {
   return {
     limitContentWidth: false,
-    ...(await load_SettingsProfileNew({ event })),
+    ...(await load_SettingsProfile({ event })),
   };
 }
