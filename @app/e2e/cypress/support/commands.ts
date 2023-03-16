80f2b6d1c039e773d0d7baa32373e46c05eebb48
Francis Nimick <fnimick@gmail.com>
2023-03-15 20:57:42 -0400

    Lengthen timeout (again) for Cypress in CI to stop false alarms

--- @app/e2e/cypress/support/commands.ts
+++ @app/e2e/cypress/support/commands.ts
@@ -155,7 +155,7 @@ Cypress.Commands.overwrite("visit", (originalFn, url) => {
   // Wait for any handlers on the target page to be registered. Otherwise we
   // could e.g. click on a form button right as the page loads, before the
   // validation is attached.
-  cy.wait(100);
+  cy.wait(Cypress.config("isInteractive") ? 500 : 1000);
 });
 
 export {}; // Make this a module so we can `declare global`
